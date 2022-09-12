0928efb815b5c459e477df9b9273136281794450
Leonardo Osorio <topette@users.noreply.github.com>
2022-09-12 16:24:52 -0500

    avance en heroes

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom"
+
 export const LoginPage = () => {
+
+  const navigate = useNavigate();
+  const logIn = () => {
+    navigate("/inicio")
+    }
+
   return (
-    <div>LoginPage</div>
+    <div className="flex h-screen justify-center items-center">
+      <button onClick={logIn} className="bg-blue-600 py-2 px-6 rounded-lg font-bold focus:outline-none text-white focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800">Ingresar 🙂 </button>
+    </div>
   )
 }
--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+import heroesApi from "../../api/HeroesApi";
+
 export const MarvelPage = () => {
+  const [getHeroes, setGetHeroes] = useState([]);
+
+  useEffect(() => {
+    obtenerHeroes();
+  }, []);
+
+  const obtenerHeroes = async () => {
+    try {
+      const respuesta = await heroesApi.get("/superheroes?tipo=heroes-marvel");
+      setGetHeroes(respuesta.data);
+      console.log(respuesta.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>MarvelPages</div>
-  )
-}
+    <>
+      <h1 className="text-center font-bold text-6xl uppercase m-4">
+        <span className="text-yellow-500">Top 20 </span>
+        <span className="text-white">Mejores Heroes de Marvel</span>
+      </h1>
+      <div className="grid sm:grid-cols-4 gap-4">
+        {getHeroes.map((heroe) => (
+          <div
+            key={heroe.id}
+            className="rounded-t-2xl shadow-2xl bg-gray-900 m-8"
+          >
+            <img src={heroe.imagen} className="rounded-2xl w-full" srcset="" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{heroe.nombre}</div>
+              <p className="text-gray-500 text-base text-justify">
+                {heroe.descripcion.substring(0, 200).concat("...")}
+              </p>
+              <Link to={`/heroe/${heroe.id}`}>
+                <button className="w-full bg-blue-500 px-8 py-2 mt-8 mb-6 rounded-lg uppercase font-bold hover:bg-blue-700">
+                  Ver perfil
+                </button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
